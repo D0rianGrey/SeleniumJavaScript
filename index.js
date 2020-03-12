61d4eaabf0587ad8101e7632943c8348a83c1bc6
eugene <utyfdfrthby08@gmail.com>
2020-03-12 22:21:15 +0200

    from pc home

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ async function example() {
   try {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("http://google.com.ua");
-    await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
+    //await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
+    await driver.findElement(By.name("q")).sendKeys("Selenium");
+    await driver.findElement(By.xpath("//div[@class='tfB0Bf']//input[@class='gNO89b']")).click();
   } catch (e) {
     console.log(e);
   }
---
+++ practice.js
@@ -0,0 +1,11 @@
+function test(name, surname) {
+    this.name = name;
+    this.surname = surname;
+
+
+
+}
+
+
+let a = new test("Eugene", "Vakerin");
+console.log(a);
