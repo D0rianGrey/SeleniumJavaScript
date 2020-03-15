ef2aa8ed927401eaaee0c20e918aca4b12e19331
eugene <utyfdfrthby08@gmail.com>
2020-03-15 13:14:33 +0200

    from pc home

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const { Builder, By, Key } = require("selenium-webdriver");
-const { assert } = require("chai");
-const { expect } = require("chai");
-const { should } = require("chai");
+const {Builder, By, Key} = require("selenium-webdriver");
+const {assert} = require("chai");
+const {expect} = require("chai");
+const {should} = require("chai");
 
 async function example() {
-  try {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("http://google.com.ua");
     //await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
     await driver.findElement(By.name("q")).sendKeys("Selenium");
-    await driver.findElement(By.xpath("//div[@class='tfB0Bf']//input[@class='gNO89b']")).click();
-  } catch (e) {
-    console.log(e);
-  }
+    await driver.findElement(By.xpath("//div[@class='tfB0Bf']//input[@class='gNO89']")).click();
+
 }
 
-example();
+example().then(x => console.log(x)).catch(err => console.log(err));
 
