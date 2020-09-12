bea0339f637e5c1c30b3cfdb9111331688fffbed
Vakerin Eugene <utyfdfrthby08@gmail.com>
2020-09-12 19:09:02 +0300

    from pc home firstTest.js

--- firstTest.js
+++ firstTest.js
@@ -6,10 +6,12 @@ const {should} = require("chai");
 async function example() {
     try {
         let driver = await new Builder().forBrowser("chrome").build();
+        await driver.manage().setTimeouts({implicit: 10000});
         await driver.get("http://google.com.ua");
         //await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
         await driver.findElement(By.name("q")).sendKeys("Selenium");
-        await driver.findElement(By.xpath("//div[@class='tfB0Bf']//input[@class='gNO89']")).click();
+        await driver.findElement(By.xpath("//div[@class='tfB0Bf']//input[@class='gNO89b']")).click();
+        await driver.close();
     } catch (e) {
         console.log(e)
     } finally {
@@ -18,5 +20,5 @@ async function example() {
 }
 
 
-example();
+example().then(() => console.log("Test is passed"));
 
