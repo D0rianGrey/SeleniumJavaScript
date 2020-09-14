da5c7dcfceca9c3535a645204b1707d355b7ce16
Vakerin Eugene <utyfdfrthby08@gmail.com>
2020-09-14 21:32:35 +0300

    1

--- tests/eighthTest.js
+++ tests/eighthTest.js
@@ -1,16 +1,15 @@
-const {Builder, By} = require("selenium-webdriver");
+const { Builder, By } = require("selenium-webdriver");
 
 const driver = new Builder().forBrowser("chrome").build();
 
-async function downloadFileTest() {
-    try {
-        await driver.get("https://rori4.github.io/selenium-practice/#/pages/practice/file-form");
-        await driver.findElement(By.xpath(""))
-
-
-    } catch (e) {
-        console.log(e);
-    }
+async function uploadFileTest() {
+  try {
+    await driver.get("https://rori4.github.io/selenium-practice/#/pages/practice/file-form");
+    await driver.findElement(By.xpath("//input[@formcontrolname='file']")).sendKeys(__dirname + "\\viber_image_2020-04-17_20-35-21.jpg");
+    await driver.findElement(By.id("submit")).click();
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-downloadFileTest();
+uploadFileTest();
